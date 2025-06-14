2a72ccb253aeab9711a34302af532718f00e73d4
yooundong-jp <imyour8572@gmail.com>
2025-06-14 16:51:30 +0900

    Gson LocalDate 인식을 위한 LocalDateAdapter class 생성

---
+++ IdeaProjects/TodoApp/src/LocalDateAdapter.java
@@ -0,0 +1,19 @@
+import com.google.gson.*;
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateAdapter implements JsonSerializer<LocalDate>, JsonDeserializer<LocalDate> {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
+
+    @Override
+    public JsonElement serialize(LocalDate date, Type type, JsonSerializationContext context) {
+        return new JsonPrimitive(date.format(formatter)); // "2025-06-14" 같은 문자열로 저장
+    }
+
+    @Override
+    public LocalDate deserialize(JsonElement json, Type type, JsonDeserializationContext context)
+            throws JsonParseException {
+        return LocalDate.parse(json.getAsString(), formatter);
+    }
+}
