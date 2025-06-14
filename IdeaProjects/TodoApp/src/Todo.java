7d0680a99c63b76caa4c8cf5d333fa70f9468f07
yooundong-jp <imyour8572@gmail.com>
2025-06-14 15:58:40 +0900

    마감일 기능 추가 Todo java

--- IdeaProjects/TodoApp/src/Todo.java
+++ IdeaProjects/TodoApp/src/Todo.java
@@ -1,15 +1,20 @@
+import java.time.LocalDate;
+
 public class Todo {
     private String task; // 할 일
-    private boolean isDone; // 끝난 일
+    private boolean isDone;// 끝난 일
+    private LocalDate dueDate;
 
-    public Todo(String task,boolean isDone) {
+    public Todo(String task,boolean isDone,LocalDate dueDate) {
         this.task = task;
         this.isDone = isDone;
+        this.dueDate = dueDate;
     }
     @Override
     public String toString() {
         String checkbox = isDone ? "[x]" : "[]";
-        return checkbox +""+ task;
+        String due = dueDate != null ? "(마감일" + dueDate + ")" : "";
+        return checkbox +""+ task +due;
     }
     public String getTask() {
         return task;
