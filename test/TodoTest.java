61821af7860e8feaa39d361919da6a4e6e8d7e8f
yooundong-jp <imyour8572@gmail.com>
2025-06-19 19:05:01 +0900

    feat(TodoTest) : testTodoCreation 카테고리 ,날짜 테스트 코드 추가 fix(TodoTest) : testMarkDone 카테고리 인자 추가

--- test/TodoTest.java
+++ test/TodoTest.java
@@ -8,14 +8,16 @@ public class TodoTest {
 
     @Test
     public void testTodoCreation() {
-        Todo todo = new Todo("공부하기", false, LocalDate.now());
+        Todo todo = new Todo("공부하기", false,"기타" ,LocalDate.now());
         assertEquals("공부하기", todo.getTask());
+        assertEquals("기타",todo.getCategory());
+        assertEquals(LocalDate.now(),todo.getDueDate());
         assertFalse(todo.isDone());
     }
 
     @Test
     public void testMarkDone() {
-        Todo todo = new Todo("운동하기", false, LocalDate.now());
+        Todo todo = new Todo("운동하기", false,"기타" ,LocalDate.now());
         todo.markDone();
         assertTrue(todo.isDone());
     }
