415ac4baca3aafa494b07d8d58498db9a94d2a48
yooundong-jp <imyour8572@gmail.com>
2025-07-10 20:26:46 +0900

    feat : Main 구현

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        TodoService service = new TodoService();
+        TodoRepository repo = new MemoryTodoRepository();
+        TodoService service = new TodoService(repo);
         TodoController controller = new TodoController(service);
 
-        System.out.println(" 무슨 일을 하실건가요?");
-        System.out.println("ex): POST /todos");
+        System.out.println(" 무슨 일을 하실건가요? 종료 -> exit");
 
         while (true) {
-            System.out.print("\n>> ");
-            String method = sc.next();
-            String uri = sc.nextLine().trim();
+            System.out.print("\n>> 입력");
+            String line = sc.nextLine().trim();
+
+            if (line.equals("exit")) {
+                break;
+            }
+            String[] parts = line.split(" ");
+            if (parts.length != 2) {
+                System.out.println("X 형식 Method/uri");
+                continue;
+            }
+            String method = parts[0];
+            String uri = parts[1];
 
             String response = controller.handleRequest(method, uri);
             System.out.println("📦 응답: " + response);
